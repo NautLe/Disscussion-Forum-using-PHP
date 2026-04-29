e5eca1413d29c0d955ae29e339e0596fc091fa27
NautLe <dauding159@gmail.com>
2026-04-29 13:52:53 +0700

    Remove secrets

--- dbConnect.php
+++ dbConnect.php
@@ -1,13 +1,22 @@
-<?php 
-$host = "";
-$dbname = 'askmecom_login';
-$username = 'askmecom';
-$password = 'ZO0r2YrisSVL';
+<?php
+
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname",
+        $username,
+        $password
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo; // Ensure the PDO object is returned
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+
+    return $pdo;
+
+} catch(PDOException $e){
+    die("Database connection failed");
 }
+?>
